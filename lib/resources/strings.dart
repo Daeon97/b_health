d58b9778f1f22d78c6dc4c1e925a337fc7737dea
Daeon97 <engelmmanuel@gmail.com>
2023-05-11 15:24:59 +0100

    Finished adding all question literals to strings.dart

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: public_member_api_docs
+// ignore_for_file: public_member_api_docs, lines_longer_than_80_chars
 
 // Routes
 const defaultRoute = '/';
@@ -69,9 +69,12 @@ const alreadyHaveAnAccountLiteral = 'Already have an account?';
 const dontHaveAnAccountLiteral = "Don't have an account?";
 const submitLiteral = 'Submit';
 const logInLiteral = 'Log In';
+const nextLiteral = 'Next';
 const forgotPasswordLiteral = 'Forgot Password?';
 const bioDataLiteral = 'Bio-data';
 const fullNameLiteral = 'Full Name';
+const addressLiteral = 'Address';
+const phoneNumberLiteral = 'Phone number';
 const dateOfBirthLiteral = 'Date of Birth';
 const genderLiteral = 'Gender';
 const bloodGroupLiteral = 'Blood Group';
@@ -96,6 +99,7 @@ const divorcedLiteral = 'Divorced';
 const widowedLiteral = 'Widowed';
 const yesLiteral = 'Yes';
 const noLiteral = 'No';
+const notSureLiteral = 'Not sure';
 const officeWorkerLiteral = 'Office Worker';
 const factoryWorkerLiteral = 'Factory Worker';
 const artisanWorkerLiteral = 'Artisan Worker';
@@ -134,21 +138,39 @@ const questionElevenLiteral = 'If yes, How many times in a week?';
 const questionTwelveLiteral = 'If you stopped, when did you start and stop?';
 const questionThirteenLiteral = 'Do you drink alcohol?';
 const questionFourteenLiteral = 'If yes, How many times in a week?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
-// const questionFiveLiteral = 'If yes, How many times in a month?';
+const questionFifteenLiteral = 'Do you notice any changes in your breast?';
+const questionSixteenLiteral =
+    'Please follow the instructions to properly examine your breast for the following changes and respond accordingly';
+const questionSeventeenHeaderLiteral = 'step 1';
+const questionSeventeenLiteral = 'take off your clothes';
+const questionEighteenHeaderLiteral = 'step 2';
+const questionEighteenLiteral = 'Put your both hands behind your head';
+const questionNineteenHeaderLiteral = 'step 3';
+const questionNineteenLiteral =
+    'Take you right hand and feel your left breast for abnormalities';
+const questionTwentyHeaderLiteral = 'step 4';
+const questionTwentyLiteral = 'Do the same for the right breast';
+const questionTwentyOneLiteral = 'Do you feel any lump in your breast?';
+const questionTwentyOneFooterLiteral = 'Visible Lump';
+const questionTwentyTwoLiteral = 'Do you experience nipple retraction?';
+const questionTwentyTwoFooterLiteral = 'Nipple Turning';
+const questionTwentyThreeLiteral = 'Do you experience breast or nipple pain?';
+const questionTwentyThreeFooterLiteral = 'Skin texture change';
+const questionTwentyFourLiteral =
+    'Have you noticed any discharge other than breast milk from the nipple?';
+const questionTwentyFourFooterLiteral = 'Nipple Discharge';
+const questionTwentyFiveLiteral =
+    'Have you noticed any redness or irritation on your breast?';
+const questionTwentyFiveFooterLiteral = 'Skin Irritation';
+const questionTwentySixLiteral =
+    'Have you noticed scaliness or thickening of the nipple or breast skin';
+const questionTwentySixFooterLiteral = 'Dimpling';
+const questionTwentySevenLiteral =
+    'Do you have a personal doctor or family doctor?';
+const questionTwentyEightLiteral =
+    'Do you wish that your information is sent to your personal doctor or family doctor?';
+const questionTwentyNineLiteral =
+    'If yes, please fill in the information of your doctor';
 
 // Special characters
 const newLine = '\n';
